7c6a2a37e4e97bbd6e850772c7b169875a5e08e8
ErwanRec <eprudhommea@ensc.fr>
2025-05-15 22:52:35 +0200

    modif global et changement de modélisation

--- projet_ENSemenCe/Terrains.cs
+++ projet_ENSemenCe/Terrains.cs
@@ -2,12 +2,12 @@
 
 public class Terrains{
     public string Nomplanete {get;set;}
-    public Maladies? MaladiesPossible {get; set;}
-    public List<Plantes>? PlanteAchetable {get;set;}
-    public Meteos? MeteoAjouté {get;set;} 
+    public string? MaladiesPossible {get; set;}
+    public List<string>? PlanteAchetable {get;set;}
+    public int[] MeteoPourcentage {get;set;} 
 
 
-    public Terrains(int numplanete, Maladies maladiesPossible, List<Plantes> planteAchetable, Meteos meteoAjouté){
+    public Terrains(int numplanete, string maladiesPossible, List<string> planteAchetable, Meteos meteoAjouté){
         string[] nomTerrains = ["Aucun","Petit Prince","Businessman","Buveur","Vaniteux","Roi","Géographe","Réverbère"];
         Nomplanete = nomTerrains[numplanete];
         MaladiesPossible = maladiesPossible;
@@ -17,22 +17,25 @@ public class Terrains{
 }
 
 public class TerrainPetitPrince:Terrains{
-    public TerrainPetitPrince():base(1,new Maladies {},new List<Plantes> {},new TempêteStellaire()){}
+    public TerrainPetitPrince():base(1,"Mildiou" ,new List<string> {},[0,0,0,6]){}
 }
 public class TerrainBusinessman:Terrains{
-    public TerrainBusinessman():base(2,new List<Maladies> {},new List<Plantes> {},new CriseEconomique()){}
+    public TerrainBusinessman():base(2,"Infertilité" ,new List<string> {},[1,1,0.8,7]){}
 }
 public class TerrainBuveur:Terrains{
-    public TerrainBuveur():base(3,new List<Maladies> {},new List<Plantes> {},new FortePluie()){}
+    public TerrainBuveur():base(3,"Grande soif" ,new List<string> {},[1,1,0.7,8]){}
+}
+public class TerrainVaniteux:Terrains{
+    public TerrainVaniteux():base(4,"Isolement" ,new List<string> {},[1,1,0.7,9]){}
 }
 public class TerrainRoi:Terrains{
-    public TerrainRoi():base(4,new List<Maladies> {},new List<Plantes> {},new Obligation()){}
+    public TerrainRoi():base(5,"Rouille" ,new List<string> {},[1,1,0.7,10]){}
 }
 public class TerrainGéographe:Terrains{
-    public TerrainGéographe():base(5,new List<Maladies> {},new List<Plantes> {},new ChangementSaison()){}
+    public TerrainGéographe():base(6,"Explosion" ,new List<string> {},[1,1,0.7,11]){}
 }
 public class TerrainRéverbère:Terrains{
-    public TerrainRéverbère():base(6,new List<Maladies> {},new List<Plantes> {},new ToutNoir()){}
+    public TerrainRéverbère():base(7,"Peur du noir" ,new List<string> {},[1,1,0.7,12]){}
 }
 
 
@@ -44,6 +47,11 @@ public class Meteos{
     public Meteos(string nom, int cata){
         Nom = nom;
         NuméroCata = cata;
+    }
+    public override string ToString()
+    {
+
+        return Nom;
     }
 } 
 public class Calme : Meteos {
@@ -109,18 +117,18 @@ public class ToutNoir : Meteos {
 public class Saisons {
     public string Nom {get;set;}
     public int[] PourcentMeteos {get;set;}
-    public Saison(string nom, int[] pourcentMeteos){
+    public Saisons(string nom, int[] pourcentMeteos){
         Nom = nom;
         PourcentMeteos = pourcentMeteos;
     }
 }
 
 public class Saison1 : Saisons{
-    public Saison1():base("Saison1",[20,20,20,20,8,5,1,0,0,0,0,0,0])//[20,20,20,20,8,5,1,1,1,1,1,1,1]
+    public Saison1():base("Saison1",[20,20,20,20,8,5,1,0,0,0,0,0,0]){}//[20,20,20,20,8,5,1,1,1,1,1,1,1]
 }
 public class Saison2 : Saisons{
-    public Saison2():base("Saison2",[20,20,20,20,5,8,1,0,0,0,0,0,0])//[20,20,20,20,5,8,1,1,1,1,1,1,1]
+    public Saison2():base("Saison2",[20,20,20,20,5,8,1,0,0,0,0,0,0]){}//[20,20,20,20,5,8,1,1,1,1,1,1,1]
 }
 public class Saison3 : Saisons{
-    public Saison3():base("Saison3",[50,15,15,15,0,0,0.8,0,0,0,0,0,0])//[50,15,15,15,0,0,0.8,0.7,0.7,0.7,0.7,0.7,0.7]
+    public Saison3():base("Saison3",[50,15,15,15,0,0,0.8,0,0,0,0,0,0]){}//[50,15,15,15,0,0,0.8,0.7,0.7,0.7,0.7,0.7,0.7]
 }
